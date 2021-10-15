df987fb5c388722a358cbfe1c3ca33375138d2f7
Paul Austen <pjaos@gmail.com>
2021-10-15 06:06:11 +0100

    Simplify the setup.py and include the web server files in the installed module.

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
-import setuptools
-
-MODULE_NAME    = "ogsolar"                                                      # The python module name
-VERSION        = "4.2"                                                          # The version of the application
-AUTHOR         = "Paul Austen"                                                  # The name of the applications author
-AUTHOR_EMAIL   = "pausten.os@gmail.com"                                         # The email address of the author
-DESCRIPTION    = "A Raspberry PI application for controlling EPEver Tracer off grid solar installations."   # A short description of the application
-LICENSE        = "MIT License"                                                  # The License that the application is distributed under
-REQUIRED_LIBS  = ['p3lib>=1.1.23','tornado>=6.1']                               # A python list of required libs (optionally including versions)
-STARTUP_SCRIPTS= ['scripts/ogsolar','scripts/tracer']                           # The command line startup scripts to be installed.
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name=MODULE_NAME,
-    version=VERSION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    description=DESCRIPTION,
-    long_description="",                                                        #This will be read from the README.md file
+setup(
+    name="ogsolar",                                  # The python module name.
+    version="4.2",                                   # The version of the module.
+    author="Paul Austen",                            # The name of the module author.
+    author_email="pausten.os@gmail.com",             # The email address of the author.
+    description="A Raspberry PI application for controlling EPEver Tracer off grid solar installations.", # A short description of the module.
+    long_description="",                             # The long description is contained in the README.md file.
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "License :: %s" % (LICENSE),
-        "Operating System :: OS Independent",
-    ],
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT License",                           # The License that the module is distributed under
+    url="https://github.com/pjaos/rpi_ogsolar",      # The home page for the module
     install_requires=[
-        REQUIRED_LIBS
+        ['p3lib>=1.1.23','tornado>=6.1'],            # A python list of required module dependencies (optionally including versions)
     ],
-    scripts=STARTUP_SCRIPTS
+    scripts=['scripts/ogsolar','scripts/tracer'],    # A list of command line startup scripts to be installed.
 )
