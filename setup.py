7a863d755e6c6ca6a8495a2399dc81ee7a51054f
Paul Austen <pjaos@gmail.com>
2021-10-17 07:36:39 +0100

    Use the laytest p3lib to ensure that syslog messages include the host IP address.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     license="MIT License",                           # The License that the module is distributed under
     url="https://github.com/pjaos/rpi_ogsolar",      # The home page for the module
     install_requires=[
-        ['p3lib>=1.1.23','tornado>=6.1','pymodbus>=2.5.2'],            # A python list of required module dependencies (optionally including versions)
+        ['p3lib>=1.1.28','tornado>=6.1','pymodbus>=2.5.2'],            # A python list of required module dependencies (optionally including versions)
     ],
     scripts=['scripts/ogsolar','scripts/tracer'],    # A list of command line startup scripts to be installed.
 )
